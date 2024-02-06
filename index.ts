4f94142d0721aab42dce26c2abec8021bfea79f5
ArielAmzallag <94179160+ArielAmzallag@users.noreply.github.com>
2024-02-06 15:30:30 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, {NextFunction, Request, Response} from "express";
+import { WeatherController } from "./weatherController";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const API_KEY = "6e758133b845476e930181014241701";
+const weatherController = new WeatherController(API_KEY);
+
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+// Route pour tester que notre api est sur la bonne voie :)
+app.get("/testApi",  (req: Request, res: Response) => {
+  res.send("Coucou, je suis dans l'API météo");
+})
+
+app.get("/weather/:city", async (req: Request, res: Response, next: NextFunction) => {
+  await weatherController.getWeather(req, res, next);
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Le server est en cours d'execution sur le port ${PORT}`);
+});
---
+++ weatherController.ts
@@ -0,0 +1,28 @@
+import {NextFunction, Request, Response } from "express";
+import axios, {AxiosResponse} from "axios";
+
+
+export class WeatherController {
+  private API_KEY: string;
+
+  constructor(apiKey: string){
+    this.API_KEY = apiKey;
+  }
+
+
+  public async getWeather(req: Request, res: Response, next: NextFunction): Promise<void>{
+    const city: string = req.params.city;
+    try{
+      const response: AxiosResponse = await axios.get(
+        `http://api.weatherapi.com/v1/current.json?key=${this.API_KEY}&q=${city}&lang=fr`
+      );
+      
+     const data = response.data;
+     res.json(data);
+    }catch(error){
+      res.status(500)
+      .json({error : "Erreur"})
+    }
+  }
+}
+  
